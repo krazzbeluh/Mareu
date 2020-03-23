58155980d83641afb563d95aae9b0cf6c5132a6c
Paul Leclerc <paulleclerc@icloud.com>
2020-03-23 15:07:21 +0100

    Synchronized RV with MeetingList

--- app/src/main/java/com/paulleclerc/mareu/ui/meetingList/MeetingListActivity.java
+++ app/src/main/java/com/paulleclerc/mareu/ui/meetingList/MeetingListActivity.java
@@ -7,12 +7,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.paulleclerc.mareu.R;
+import com.paulleclerc.mareu.model.Meeting;
 import com.paulleclerc.mareu.ui.addMeeting.AddMeetingActivity;
 
 public class MeetingListActivity extends AppCompatActivity {
+    private static final String TAG = MeetingListActivity.class.getSimpleName();
 
     private RecyclerView mRecyclerView;
     private MeetingListRecyclerViewAdapter mAdapter;
@@ -33,9 +36,19 @@ public class MeetingListActivity extends AppCompatActivity {
         mAdapter = new MeetingListRecyclerViewAdapter();
         mRecyclerView.setAdapter(mAdapter);
     }
+    
+    private void reloadList() {
+        mAdapter.notifyItemInserted(Meeting.getMeetingList().size() - 1);
+    }
 
     public void addMeeting(View view) {
         Intent intent = new Intent(MeetingListActivity.this, AddMeetingActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        reloadList();
     }
 }
