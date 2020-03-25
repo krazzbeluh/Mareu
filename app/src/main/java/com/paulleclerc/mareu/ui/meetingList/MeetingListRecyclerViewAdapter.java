f1d1a31ba857cbc116ce66fca04e0b7a41a589f9
Paul Leclerc <paulleclerc@icloud.com>
2020-03-25 16:29:15 +0100

    Added textview autoscroll

--- app/src/main/java/com/paulleclerc/mareu/ui/meetingList/MeetingListRecyclerViewAdapter.java
+++ app/src/main/java/com/paulleclerc/mareu/ui/meetingList/MeetingListRecyclerViewAdapter.java
@@ -1,9 +1,11 @@
 package com.paulleclerc.mareu.ui.meetingList;
 
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -59,7 +61,7 @@ public class MeetingListRecyclerViewAdapter extends RecyclerView.Adapter<Meeting
 
         void bindMeeting(Meeting meeting) {
             this.mMeeting = meeting;
-            String title = meeting.getSubject() + " - " + meeting.getLocation();
+            String title = meeting.getSubject() + " - " + meeting.getLocation() + " - " + meeting.getDateFormatted();
             mTitleView.setText(title);
             mSubtitleView.setText(meeting.getParticipants());
             mThumbnailView.setBackgroundTintList(itemView.getResources().getColorStateList(meeting.getColor()));
